1a6ac0874f74bf7cc45f3ccc829eb1e4c56c7e88
gentry <gma23@txstate.edu>
2022-08-25 21:01:48 -0500

    all aboard, datasets

--- src/main.py
+++ src/main.py
@@ -22,9 +22,9 @@ CLEANUP = True
 
 #Dataset are returned in channels-last format
 datasets = {
-    #'unimib' :  unimib_load_dataset,
+    'unimib' :  unimib_load_dataset,
     #'twister' : e4_load_dataset,
-    #'uci har' : uci_har_load_dataset,
+    'uci har' : uci_har_load_dataset,
     'sussex huawei' : sh_loco_load_dataset
 }
 
