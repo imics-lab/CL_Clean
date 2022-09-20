771ffd33cab755d3679877f912cb0f5a51d1702d
gentry <gma23@txstate.edu>
2022-09-20 10:21:04 -0500

    made a nice generalized early stopper

--- src/early_stopping.py
+++ src/early_stopping.py
@@ -7,25 +7,36 @@
 import numpy as np
 
 
-class EarlyStopping_on_loss_minimum():
+class EarlyStopping():
     """
     Set early_stop flag to True if tolerance cycles have passed without adeqate change
     """
-    def __init__(self, tolerance=5, min_delta=0.01):
-
+    def __init__(self, tolerance=5, min_delta=0.01, mode='maximum'):
+        assert mode in ['minimum', 'maximum'], "Mode must be minimum or maximum"
         self.patience = tolerance
         self.min_delta = min_delta
         self.counter = 0
         self.early_stop = False
         self.losses = list()
+        self.mode = mode
 
-    def __call__(self, train_loss):
-        self.losses.append(train_loss)
-        if len(self.losses) == self.patience:
-            if np.nanmax(self.losses) - train_loss < self.min_delta:
-                self.counter += 1
-            else:
-                self.counter = 0
-            if self.counter == self.patience:
+    def __call__(self, value):
+        self.losses.append(value)
+        if self.mode == 'minimum': 
+            if len(self.losses) == self.patience:
+                if np.nanmax(self.losses) - value < self.min_delta:
+                    self.counter += 1
+                else:
+                    self.counter = 0
+                self.losses = self.losses[1:]
+        else:
+            if len(self.losses) == self.patience:
+                if value - np.nanmax(self.losses)  < self.min_delta:
+                    self.counter += 1
+                else:
+                    self.counter = 0
+                self.losses = self.losses[1:]
+
+        if self.counter == self.patience:
                 self.early_stop = True
-            self.losses = self.losses[1:]
+        
