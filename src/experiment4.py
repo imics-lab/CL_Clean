a1a76e859ab5045f731eda3e34b06cc59279efa7
Gentry Atkinson <gma23@txstate.edu>
2023-04-07 15:50:24 -0500

    running lf on twristar

--- src/experiment4.py
+++ src/experiment4.py
@@ -11,6 +11,7 @@ from sklearn.metrics import precision_score
 from model_wrappers import *
 from early_stopping import EarlyStopping
 from labelfix.src.labelfix import check_dataset
+from tensorflow import one_hot, make_ndarray
 
 feature_learners = {
     "traditional" : Engineered_Features,
@@ -55,7 +56,11 @@ def exp_4(
         'noise percent' : [],
         '# instances' : [],
         'number mislabeled' : [],
-        'prec of labels' : []
+        'len of return' : [],
+        'num mislabeled' : [],
+        'prec of labels at 1%' : [],
+        'prec of labels at 2%' : [],
+        'prec of labels at 3%' : []
     }
 
     print ("Running Experiment 4  on ", set)
@@ -110,19 +115,35 @@ def exp_4(
                 print("No label sets found. Please run experiments 1 and 2 first")
                 return results
             
-            labfix_results = check_dataset(f_train, y_train_noisy)
+            #y_hot = np.eye(num_classes, dtype=int)[y_train_noisy] #cast to one-hot without tf
+            labfix_results = check_dataset(f_train, y_train_noisy, 
+                                           hyperparams={
+                                            "input_dim": f_train.shape[1],
+                                            "output_dim": num_classes,
+                                            "num_hidden": 3,
+                                            "size_hidden": 120,
+                                            "dropout": 0.1,
+                                            "epochs": 400,
+                                            "learn_rate": 1e-2,
+                                            "activation": "relu"
+            })
+            num_mislabeled = np.count_nonzero(y_train_noisy != y_train)
+            mislabeled_indices = np.where(y_train_noisy != y_train)
             sus_indices = np.array(labfix_results['indices'])
-            one_p = len(sus_indices) // 100
+            one_p = (len(sus_indices) // 100)
             np.save(f'temp/{set}_{extractor}_{noise_level}_sus_indices.npy', sus_indices)
-            prec_1p = np.count_nonzero(sus_indices[:one_p] in np.where(y_train_noisy != y_train)) / one_p
-            prec_2p = np.count_nonzero(sus_indices[:one_p*2] in np.where(y_train_noisy != y_train)) / one_p*2
-            prec_3p = np.count_nonzero(sus_indices[:one_p*3] in np.where(y_train_noisy != y_train)) / one_p*3
+            prec_1p = np.count_nonzero([np.isin(i, mislabeled_indices) for i in sus_indices[:one_p]]) / one_p
+            prec_2p = np.count_nonzero([np.isin(i, mislabeled_indices)  for i in sus_indices[:one_p*2]]) / one_p*2
+            prec_3p = np.count_nonzero([np.isin(i, mislabeled_indices) for i in sus_indices[:one_p*3]]) / one_p*3
 
             results['set'].append(set)
             results['features'].append(extractor)
             results['noise percent'].append(noise_dic[noise_level]['percent'])
             results['# instances'].append(y_test.shape[0])
             results['number mislabeled'].append(np.count_nonzero(y_train != y_train_noisy))
+            results['len of return'].append(len(sus_indices))
+            results['num mislabeled'].append(num_mislabeled)
             results['prec of labels at 1%'].append(prec_1p)
             results['prec of labels at 2%'].append(prec_2p)
             results['prec of labels at 3%'].append(prec_3p)
+    return results
